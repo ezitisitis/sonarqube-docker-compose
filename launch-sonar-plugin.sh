51cc83221037afeb453764adcb894b0e830ab8eb
Marks Bogdanovs <git@ezitisitis.com>
2021-07-12 22:49:16 +0300

    Change Docker host configuration

--- launch-sonar-plugin.sh
+++ launch-sonar-plugin.sh
@@ -3,4 +3,9 @@
 cp .env.example .env
 vi .env
 
+sysctl -w vm.max_map_count=262144
+sysctl -w fs.file-max=65536
+ulimit -n 65536
+ulimit -u 4096
+
 docker-compose -f docker-compose.yml -f docker-compose.plugin.yml up -d
--- launch-sonar.sh
+++ launch-sonar.sh
@@ -3,4 +3,9 @@
 cp .env.example .env
 vi .env
 
+sysctl -w vm.max_map_count=262144
+sysctl -w fs.file-max=65536
+ulimit -n 65536
+ulimit -u 4096
+
 docker-compose up -d
